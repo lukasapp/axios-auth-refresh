faae121201689d18c6db9586a1ef59c1c604844f
Lukas Reichert <lukas.reichert@showpad.com>
2024-05-28 14:58:08 +0200

    Add dist

---
+++ dist/model.d.ts
@@ -0,0 +1,30 @@
+import { AxiosError, AxiosInstance, InternalAxiosRequestConfig } from 'axios';
+export interface AxiosAuthRefreshOptions {
+    statusCodes?: Array<number>;
+    /**
+     * Determine whether to refresh, if "shouldRefresh" is configured, The "statusCodes" logic will be ignored
+     * @param error AxiosError
+     * @returns boolean
+     */
+    shouldRefresh?(error: AxiosError): boolean;
+    retryInstance?: AxiosInstance;
+    interceptNetworkError?: boolean;
+    pauseInstanceWhileRefreshing?: boolean;
+    onRetry?: (
+        requestConfig: InternalAxiosRequestConfig
+    ) => InternalAxiosRequestConfig | Promise<InternalAxiosRequestConfig>;
+    /**
+     * @deprecated
+     * This flag has been deprecated in favor of `pauseInstanceWhileRefreshing` flag.
+     * Use `pauseInstanceWhileRefreshing` instead.
+     */
+    skipWhileRefreshing?: boolean;
+}
+export interface AxiosAuthRefreshCache {
+    skipInstances: AxiosInstance[];
+    refreshCall: Promise<any> | undefined;
+    requestQueueInterceptorId: number | undefined;
+}
+export interface AxiosAuthRefreshRequestConfig extends InternalAxiosRequestConfig {
+    skipAuthRefresh?: boolean;
+}
